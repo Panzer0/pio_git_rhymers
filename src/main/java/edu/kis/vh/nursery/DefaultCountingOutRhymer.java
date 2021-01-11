cfe88d3bc8e661e4e569a15fa9529f8176f577eb
Daniel Wieczorek <222606@edu.p.lodz.pl>
2021-01-11 20:14:41 +0100

    Błędy formatowania i konwencji. | Podpunkt 4 | Korekcja nazw metod i klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,6 +1,6 @@
 package edu.kis.vh.nursery;
 
-public class defaultCountingOutRhymer {
+public class DefaultCountingOutRhymer {
 
 	private int[] NUMBERS = new int[12];
 
@@ -11,7 +11,7 @@ public class defaultCountingOutRhymer {
 			NUMBERS[++total] = in;
 	}
 
-		public boolean CallCheck() {
+		public boolean callCheck() {
 			return total == -1;
 		}
 		
@@ -20,13 +20,13 @@ public class defaultCountingOutRhymer {
 			}
 		
 				protected int peekaboo() {
-					if (CallCheck())
+					if (callCheck())
 						return -1;
 					return NUMBERS[total];
 				}
 			
 					public int countOut() {
-						if (CallCheck())
+						if (callCheck())
 							return -1;
 						return NUMBERS[total--];
 					}
