077f4b2e3d14f97ebb07b686b02b0da47ef2cbc6
Daniel Wieczorek <222606@edu.p.lodz.pl>
2021-01-18 22:49:48 +0100

    1.5 | Dodano dokumentację klasy IntLinkedList

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,9 +1,18 @@
 package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
-									//Nowy folder
+	/**
+	 * Klasa reprezentuje listę dwukierunkową
+	 * zawierającą elementy typu int.
+	 */
+
 	Node last;
-	int i;
+	/**
+	 * last to ostatni element listy
+	 */
+
+
+	//int i;
 
 	public void push(int i) {
 		if (last == null)
@@ -14,20 +23,38 @@ public class IntLinkedList {
 			last = last.next;
 		}
 	}
+	/**
+	 *	push dodaje element na koniec listy
+	 */
 
 	public boolean isEmpty() {
 		return last == null;
 	}
 
+	/**
+	 * isEmpty sprawdza, czy lista jest pusta
+	 * Jeśli jest, zwraca 1, jeśli nie, 0
+	 */
+
+
 	public boolean isFull() {
 		return false;
 	}
+	/**
+	 * isFull sprawdza, czy lista jest pełna
+	 * Jeśli jest, zwraca 1, jeśli nie, 0
+	 */
+
 
 	public int top() {
 		if (isEmpty())
 			return -1;
 		return last.value;
 	}
+	/**
+	 * top zwraca ostatni element listy
+	 * lub -1, gdy ta jest pusta
+	 */
 
 	public int pop() {
 		if (isEmpty())
@@ -36,5 +63,10 @@ public class IntLinkedList {
 		last = last.prev;
 		return ret;
 	}
+	/**
+	 * pop zwraca ostatni element listy
+	 * lub -1, gdy ta jest pusta.
+	 * Dodatkowo usuwa ten element.
+	 */
 
 }
