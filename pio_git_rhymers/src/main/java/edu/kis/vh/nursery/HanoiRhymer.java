7cc2c2d8400ed8d827ad8e875546e0a11380cd9f
Daniel Wieczorek <222606@edu.p.lodz.pl>
2021-01-18 02:35:21 +0100

    1.4 | Temat: accessory i hermetyzacja | 2. Dodano getter i setter dla zmiennej total

---
+++ pio_git_rhymers/src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -0,0 +1,26 @@
+package edu.kis.vh.nursery;
+
+public class HanoiRhymer extends DefaultCountingOutRhymer {
+
+    private int totalRejected = 0;
+
+    public int reportRejected() {
+        return totalRejected;
+    }
+
+    public int getTotalRejected() {
+        return totalRejected;
+    }
+
+    public void setTotalRejected(int totalRejected) {
+        this.totalRejected = totalRejected;
+    }
+
+    public void countIn(int in) {
+        if (!callCheck() && in > peekaboo())
+            totalRejected++;
+        else
+            super.countIn(in);
+    }
+}
+//Alt + strzałki przełączają w IntelliJ IDEA aktywny plik.
