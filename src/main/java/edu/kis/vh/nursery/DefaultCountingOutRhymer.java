9177de460a9c148fef603b0b3a6b9e97bdbf000a
Daniel Wieczorek <222606@edu.p.lodz.pl>
2021-01-26 10:50:36 +0100

    2.1 | 2. Created constructors for the new contents of DefaultCountingOutRhymer, also accidentally removed the test files, will fix that shortly

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,47 +2,13 @@ package edu.kis.vh.nursery;
 
 
 public class DefaultCountingOutRhymer {
-	private final int EMPTY_RHYMER_INDICATOR=-1;
-	private final int DEFAULT_VALUE=-1;
-	private final int MAX_SIZE=12;
-	private static final int variable = -1;
-	private int[] numbers = new int[MAX_SIZE];
+	IntArrayStack test;
 
-	public int getTotal() {
-		return total;
+	public DefaultCountingOutRhymer(IntArrayStack test) {
+		this.test = test;
 	}
+	public DefaultCountingOutRhymer(){
 
-	private int total = -1;
-
-	public void countIn(int in) {
-		if (!isFull())
-			numbers[++total] = in;
 	}
-
-	public int[] getNumbers() {
-		return numbers;
-	}
-
-	public boolean callCheck() {
-			return total == EMPTY_RHYMER_INDICATOR;
-		}
-
-	public boolean isFull() {
-		return total==(MAX_SIZE-1) ;
-	}
-		
-
-	protected int peekaboo() {
-		if (callCheck())
-			return DEFAULT_VALUE;
-		return numbers[total];
-	}
-			
-	public int countOut() {
-		if (callCheck())
-			return variable;
-		return numbers[total--];
-	}
-
 }
 
